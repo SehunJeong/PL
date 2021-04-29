749c3d0286da31e0485740b14bfb6c86c68f494a
Sehun Jeong <jeongsehun@gmail.com>
2021-04-30 08:12:39 +0900

    Problem 5

--- HW1/Problem_5.ml
+++ HW1/Problem_5.ml
@@ -1,5 +1,5 @@
 let rec iter : int * (int -> int) -> (int -> int) = fun (n, f) ->
     if n = 0 then fun x -> x
-    else fun x -> iter(n-1, f) x;;
+    else fun x -> iter(n-1, f) (f x);;
 
 print_endline (string_of_int(iter(3, fun x -> 2+x) 0));
