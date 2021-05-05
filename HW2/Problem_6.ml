862554725facdbcce3983df6cf0c8c90fd9bccc4
Sehun Jeong <jeongsehun@gmail.com>
2021-05-05 11:44:43 +0900

    Problem 6

--- HW2/Problem_6.ml
+++ HW2/Problem_6.ml
@@ -1,5 +1,10 @@
-let reduce: ('a -> 'b -> 'c -> 'c) -> 'a list -> 'b list -> 'c -> 'c = fun f l1 l2 i ->
-    i;;
+let rec reduce: ('a -> 'b -> 'c -> 'c) -> 'a list -> 'b list -> 'c -> 'c = fun f l1 l2 i ->
+    assert((List.compare_lengths l1 l2) = 0);
+    match l1, l2 with
+    | [], l2' -> 0
+    | l1', [] -> 0
+    | [x], [y] -> f x y i
+    | x::l1', y::l2' -> reduce f l1' l2' (f x y i);;
 
-reduce (fun x y z -> x * y + z) [1;2;3] [0;1;2] 0;;
+print_endline(string_of_int(reduce (fun x y z -> x * y + z) [1;2;3] [0;1;2] 0));;
 
