bc8791b3318640c4f008da804eb2bcbfea0809bb
Sehun Jeong <jeongsehun@gmail.com>
2021-05-11 08:21:22 +0900

    HW3: language outline

---
+++ HW3/lang.ml
@@ -0,0 +1,39 @@
+type program = exp
+and exp =
+  | UNIT
+  | TRUE
+  | FALSE
+  | CONST of int
+  | VAR of var
+  | ADD of exp * exp
+  | SUB of exp * exp
+  | MUL of exp * exp
+  | DIV of exp * exp
+  | EQUAL of exp * exp
+  | LESS of exp * exp
+  | NOT of exp
+  | NIL
+  | CONS of exp * exp
+  | APPEND of exp * exp
+  | HEAD of exp
+  | TAIL of exp
+  | ISNIL of exp
+  | IF of exp * exp * exp
+  | LET of var * exp * exp
+  | LETREC of var * var * exp * exp
+  | LETMREC of (var * var * exp) * (var * var * exp) * exp
+  | PROC of var * exp
+  | CALL of exp * exp
+  | PRINT of exp
+  | SEQ of exp * exp
+and var = string;;
+
+type value =
+  | Unit
+  | Int of int
+  | Bool of bool
+  | List of value list
+  | Procedure of var * exp * env
+  | RecProcedure of var * var * exp * env
+  | MRecProcedure of (* complete this definition *)
+and env = (var * value) list;;
