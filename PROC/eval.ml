b2d278f7ff971e3d66da3684f32bcb84128f586b
Sehun Jeong <jeongsehun@gmail.com>
2021-12-14 17:03:43 +0900

    ING: MRecProc

--- PROC/eval.ml
+++ PROC/eval.ml
@@ -37,17 +37,23 @@ let rec eval: exp -> env -> value
     eval e2 (extend_env (f, rp) env)
   | LETMREC (f, x1, f_e, g, x2, g_e, e) ->
     (*FIXME: *)
-    let rp1 = RecProc (f, x1, f_e, extend_env (g, RecProc(g, x2, g_e, env)) env) in
+    let mrp = MRecProc (f, x1, f_e, g, x2, g_e, env) in
+    eval e (extend_env (g, mrp) (extend_env (f, mrp) env))
+    (*let rp1 = RecProc (f, x1, f_e, extend_env (g, RecProc(g, x2, g_e, env)) env) in
     let rp2 = RecProc (g, x2, g_e, extend_env (f, RecProc(f, x1, f_e, env)) env) in
     let env' = extend_env (f, rp1) env in
     let env'' = extend_env (g, rp2) env' in
-    eval e env''   (* SEQ ("odd", 13) in env *)
+    eval e env''   (* SEQ ("odd", 13) in env *) *)
   | SEQ (e1, e2) ->
     let v = eval e2 env in
     (match eval e1 env with
     | Proc (x, e, env') -> 
       eval e (extend_env (x, v) env')
-    | RecProc (f, x, e, env') ->
-      let rp = RecProc (f, x, e, env') in
-      eval e (extend_env (x, v) (extend_env (f, rp) env'))
+    | RecProc (_, x, e, _) ->
+      (*let rp = RecProc (f, x, e, env') in*)
+      eval e (extend_env (x, v) env)
+    | MRecProc (f, x1, f_e, g, x2, g_e, _) ->
+      (match e1 with
+      | VAR fg -> if fg = f then eval f_e (extend_env (x1, v) env) else (if fg = g then eval g_e (extend_env (x2, v) env) else raise (Failure ("Function not found")))
+      | _ -> raise (Failure ("Function not found")))
     | _ -> raise (Failure "Type Error: Sequence must begin with an expression that implies a Proc, RecProc, or MRecProc type object.")) 
