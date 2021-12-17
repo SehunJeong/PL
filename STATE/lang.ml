7bfe7aab76d17485f0dabecdde1c101f567c4e3d
Sehun Jeong <jeongsehun@gmail.com>
2021-12-17 14:49:50 +0900

    ING: STATE

---
+++ STATE/lang.ml
@@ -0,0 +1,14 @@
+type program = exp
+and exp =
+    | CONST of int
+    | VAR of var
+    | ADD of exp * exp
+    | SUB of exp * exp
+    | READ
+    | ISZERO of exp
+    | IF of exp * exp * exp
+    | LET of var * exp * exp
+    | LETREC of var * var * exp * exp
+    | PROC of var * exp
+    | APPLY of exp * exp
+and var = string
