d56ef09ff1bf98d1c18e88ebba7457f9ff579774
Sehun Jeong <jeongsehun@gmail.com>
2022-08-17 16:07:30 +0900

    Clean-ups

--- TYPE/AUTOMATIC/domain.ml
+++
@@ -1,9 +0,0 @@
-open Lang
-
-let rec string_of_type: typ -> string
-= fun v ->
-  match v with
-  | TyInt -> "TyInt"
-  | TyBool -> "TyBool"
-  | TyFun (t1, t2) -> string_of_type t1 ^ " -> " ^ string_of_type t2
-  | TyVar tyvar -> tyvar
